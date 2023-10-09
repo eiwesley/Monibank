cedf324cb22a0c666e5491dcf501b4f5aead6c41
Wesley Medeiros <90868789+eiwesley@users.noreply.github.com>
2023-10-09 16:10:58 -0300

    :sparkles: feat: 2 CPF- primeiro dígito

--- pages/js/valida-CPF.js
+++ pages/js/valida-CPF.js
@@ -1,11 +1,12 @@
 export default function ehUmCPF(campo) {
     const cpf = campo.value.replace(/\.|-/g, "");
     validaNumerosRepetidos(cpf);
+    validaPrimeiroDigito(cpf);
 
     console.log(validaNumerosRepetidos(cpf))
 }
 
-function validaNumerosRepetidos(cpf){
+function validaNumerosRepetidos(cpf) {
     const numerosRepetidos = [
         '00000000000',
         '11111111111',
@@ -20,4 +21,22 @@ function validaNumerosRepetidos(cpf){
     ]
 
     return numerosRepetidos.includes(cpf);
+}
+
+function validaPrimeiroDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 10;
+
+    for (let tamanho = 0; tamanho < 9; tamanho++) {
+        soma = cpf[tamanho] * multiplicador;
+        multiplicador--;
+    }
+
+    soma = (soma * 10) % 11;
+
+    if (soma == 10 || soma == 11) {
+        soma = 0;
+    }
+
+    return soma != cpf[9];
 }
