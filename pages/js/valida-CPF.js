57f59a00b30e88c5aba5f22df2c88619b282dcf1
Wesley Medeiros <90868789+eiwesley@users.noreply.github.com>
2023-10-09 16:17:36 -0300

    :sparkles: feat: 3 CPF- segundo dígito

--- pages/js/valida-CPF.js
+++ pages/js/valida-CPF.js
@@ -1,9 +1,10 @@
 export default function ehUmCPF(campo) {
     const cpf = campo.value.replace(/\.|-/g, "");
-    validaNumerosRepetidos(cpf);
-    validaPrimeiroDigito(cpf);
-
-    console.log(validaNumerosRepetidos(cpf))
+    if(validaNumerosRepetidos(cpf) || validaPrimeiroDigito(cpf) || validaSegundoDigito(cpf)){
+        console.log("Esse CPF não existe!");
+    }else{
+        console.log("Existe");
+    }
 }
 
 function validaNumerosRepetidos(cpf) {
@@ -28,7 +29,7 @@ function validaPrimeiroDigito(cpf) {
     let multiplicador = 10;
 
     for (let tamanho = 0; tamanho < 9; tamanho++) {
-        soma = cpf[tamanho] * multiplicador;
+        soma += cpf[tamanho] * multiplicador;
         multiplicador--;
     }
 
@@ -39,4 +40,22 @@ function validaPrimeiroDigito(cpf) {
     }
 
     return soma != cpf[9];
+}
+
+function validaSegundoDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 11;
+
+    for (let tamanho = 0; tamanho < 10; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--;
+    }
+
+    soma = (soma * 10) % 11;
+
+    if (soma == 10 || soma == 11) {
+        soma = 0;
+    }
+
+    return soma != cpf[10];
 }
